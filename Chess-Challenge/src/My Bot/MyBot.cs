e68679fe2ff0ddb8159a0885642cc7ad942a5926
Mattis Boeckle <mattisderbock@gmail.com>
2023-07-29 16:01:02 +0200

    Implement basic negamax and eval functions

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,71 @@
 ﻿using ChessChallenge.API;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Data;
 
 public class MyBot : IChessBot
 {
-    public Move Think(Board board, Timer timer)
+    static readonly int[] PIECE_VALUES = { 100, 320, 350, 500, 900, 20000 };
+
+    Board board;
+    Timer timer;
+
+    Move bestMove;
+    public Move Think(Board _board, Timer _timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        board = _board;
+        timer = _timer;
+
+        bestMove = Move.NullMove;
+        int max = -99999;
+
+        foreach (var move in board.GetLegalMoves())
+        {
+            board.MakeMove(move);
+            int score = -Search(3);
+            board.UndoMove(move);
+
+            if (score > max)
+            {
+                bestMove = move;
+                max = score;
+            }
+        }
+
+        return bestMove;
+    }
+
+    int Search(int depth)
+    {
+        if (depth == 0) return Evaluate();
+
+        int max = -99999;
+        foreach (Move move in board.GetLegalMoves())
+        {
+            board.MakeMove(move);
+            int score = -Search(depth - 1);
+            board.UndoMove(move);
+            if (score > max)
+            {
+                max = score;
+            }
+        }
+        return max;
+    }
+
+    int Evaluate()
+    {
+        int sideScaleFactor = (board.IsWhiteToMove ? 1 : -1);
+        if (board.IsDraw()) return 0;
+        if (board.IsInCheckmate()) return 100000 * sideScaleFactor;
+
+        int eval = 0;
+        PieceList[] pieces = board.GetAllPieceLists();
+        for (int i = 0; i < pieces.Length / 2; i++)
+        {
+            eval += PIECE_VALUES[i] * (pieces[i].Count - pieces[i + 6].Count);
+        }
+        
+        return sideScaleFactor * eval;
     }
 }
